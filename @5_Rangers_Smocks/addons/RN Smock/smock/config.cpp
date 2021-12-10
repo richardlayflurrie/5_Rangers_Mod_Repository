b07822035ecd3d3fb1a6456675d714980b53abcf
richardlayflurrie <59142618+richardlayflurrie@users.noreply.github.com>
2021-12-10 18:29:48 +0000

    Fixed Missing Empty Folder Preventing Textures Working (?)

---
+++ @5_Rangers_Smocks/addons/RN Smock/smock/config.cpp
@@ -0,0 +1,579 @@
+//This is an edit of the Royal Navy Smock by Phantom Hawk
+
+class CfgPatches
+{
+	class RN_Smock
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 1.0;
+		requiredAddons[]=
+		{
+			"A3_Characters_F",
+			"A3_Characters_F_OPFOR",
+			"A3_Characters_F_BLUFOR",
+			"A3_Characters_F_INDEP",
+			"A3_Characters_F_Common",
+			"A3_Characters_F_Civil",
+			"A3_Characters_F_Heads",
+			"A3_Characters_F_Common",
+			"A3_Data_F"
+		};
+    };
+};
+
+class CfgVehicles
+{
+    class UK3CB_BAF_Soldier_Smock_Base;
+	
+	class B_RM_Smock_Pte: UK3CB_BAF_Soldier_Smock_Base
+	{
+		author="Steel";
+		scope=1;
+		scopeCurator=0;
+		displayName="Soldier";
+		uniformClass="U_B_RN_Smock";
+		hiddenSelections[]=
+		{
+			"camo1"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\smock_mtp_co_pte.paa"
+		};
+	};
+	
+	class B_RM_Smock_LCpl: UK3CB_BAF_Soldier_Smock_Base
+	{
+		author="Steel";
+		scope=1;
+		scopeCurator=0;
+		displayName="Soldier";
+		uniformClass="U_B_RN_Smock";
+		hiddenSelections[]=
+		{
+			"camo1"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\smock_mtp_co_lcpl.paa"
+		};
+	};
+	
+	class B_RM_Smock_Cpl: UK3CB_BAF_Soldier_Smock_Base
+	{
+		author="Steel";
+		scope=1;
+		scopeCurator=0;
+		displayName="Soldier";
+		uniformClass="U_B_RN_Smock";
+		hiddenSelections[]=
+		{
+			"camo1"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\smock_mtp_co_cpl.paa"
+		};
+	};
+	
+	class B_RM_Smock_Sgt: UK3CB_BAF_Soldier_Smock_Base
+	{
+		author="Steel";
+		scope=1;
+		scopeCurator=0;
+		displayName="Soldier";
+		uniformClass="U_B_RN_Smock";
+		hiddenSelections[]=
+		{
+			"camo1"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\smock_mtp_co_sgt.paa"
+		};
+	};
+	
+	class B_RM_Smock_CSgt: UK3CB_BAF_Soldier_Smock_Base
+	{
+		author="Steel";
+		scope=1;
+		scopeCurator=0;
+		displayName="Soldier";
+		uniformClass="U_B_RN_Smock";
+		hiddenSelections[]=
+		{
+			"camo1"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\smock_mtp_co_csgt.paa"
+		};
+	};	
+	
+	class B_RM_Smock_WO2: UK3CB_BAF_Soldier_Smock_Base
+	{
+		author="Steel";
+		scope=1;
+		scopeCurator=0;
+		displayName="Soldier";
+		uniformClass="U_B_RN_Smock";
+		hiddenSelections[]=
+		{
+			"camo1"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\smock_mtp_co_wo2.paa"
+		};
+	};
+	
+	class B_RM_Smock_WO1: UK3CB_BAF_Soldier_Smock_Base
+	{
+		author="Steel";
+		scope=1;
+		scopeCurator=0;
+		displayName="Soldier";
+		uniformClass="U_B_RN_Smock";
+		hiddenSelections[]=
+		{
+			"camo1"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\smock_mtp_co_wo1.paa"
+		};
+	};
+	
+	class B_RM_Smock_2Lt: UK3CB_BAF_Soldier_Smock_Base
+	{
+		author="Steel";
+		scope=1;
+		scopeCurator=0;
+		displayName="Soldier";
+		uniformClass="U_B_RN_Smock";
+		hiddenSelections[]=
+		{
+			"camo1"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\smock_mtp_co_2lt.paa"
+		};
+	};
+	
+	class B_RM_Smock_Lt: UK3CB_BAF_Soldier_Smock_Base
+	{
+		author="Steel";
+		scope=1;
+		scopeCurator=0;
+		displayName="Soldier";
+		uniformClass="U_B_RN_Smock";
+		hiddenSelections[]=
+		{
+			"camo1"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\smock_mtp_co_lt.paa"
+		};
+	};
+	
+	class B_RM_Smock_Capt: UK3CB_BAF_Soldier_Smock_Base
+	{
+		author="Steel";
+		scope=1;
+		scopeCurator=0;
+		displayName="Soldier";
+		uniformClass="U_B_RN_Smock";
+		hiddenSelections[]=
+		{
+			"camo1"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\smock_mtp_co_capt.paa"
+		};
+	};
+	
+	class B_RM_Smock_Maj: UK3CB_BAF_Soldier_Smock_Base
+	{
+		author="Steel";
+		scope=1;
+		scopeCurator=0;
+		displayName="Soldier";
+		uniformClass="U_B_RN_Smock";
+		hiddenSelections[]=
+		{
+			"camo1"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\smock_mtp_co_maj.paa"
+		};
+	};
+	
+	class B_RM_Smock_LtCol: UK3CB_BAF_Soldier_Smock_Base
+	{
+		author="Steel";
+		scope=1;
+		scopeCurator=0;
+		displayName="Soldier";
+		uniformClass="U_B_RN_Smock";
+		hiddenSelections[]=
+		{
+			"camo1"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\smock_mtp_co_ltcol.paa"
+		};
+	};
+	
+	class B_RM_Smock_Col: UK3CB_BAF_Soldier_Smock_Base
+	{
+		author="Steel";
+		scope=1;
+		scopeCurator=0;
+		displayName="Soldier";
+		uniformClass="U_B_RN_Smock";
+		hiddenSelections[]=
+		{
+			"camo1"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\smock_mtp_co_col.paa"
+		};
+	};		
+	
+	class B_RM_Smock_SLt: UK3CB_BAF_Soldier_Smock_Base
+	{
+		author="Steel";
+		scope=1;
+		scopeCurator=0;
+		displayName="Soldier";
+		uniformClass="U_B_RN_Smock";
+		hiddenSelections[]=
+		{
+			"camo1"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\smock_mtp_co_slt.paa"
+		};
+	};	
+	
+	class B_RM_Smock_LtRN: UK3CB_BAF_Soldier_Smock_Base
+	{
+		author="Steel";
+		scope=1;
+		scopeCurator=0;
+		displayName="Soldier";
+		uniformClass="U_B_RN_Smock";
+		hiddenSelections[]=
+		{
+			"camo1"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\smock_mtp_co_ltrn.paa"
+		};
+	};
+};
+
+class CfgWeapons
+{
+	class Uniform_Base;
+	class UniformItem;
+	class InventoryItem_Base_F;
+	class ItemCore;
+	class HeadGearItem;
+	class UK3CB_BAF_U_Smock_MTP;
+	
+	class U_B_RM_Smock_Pte: UK3CB_BAF_U_Smock_MTP
+	{
+		author="Steel";
+		scope=2;
+		displayName="Smock [Private]";
+		model="\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\suitpack_soldier_RN_co.paa"
+		};
+		class ItemInfo: UniformItem
+		{
+			uniformModel="-";
+			uniformClass="B_RM_Smock_Pte";
+			containerClass="Supply40";
+			mass=20;
+		};
+	};
+	
+	class U_B_RM_Smock_LCpl: UK3CB_BAF_U_Smock_MTP
+	{
+		author="Steel";
+		scope=2;
+		displayName="Smock [Lance Corporal]";
+		model="\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\suitpack_soldier_RN_co.paa"
+		};
+		class ItemInfo: UniformItem
+		{
+			uniformModel="-";
+			uniformClass="B_RM_Smock_LCpl";
+			containerClass="Supply40";
+			mass=20;
+		};
+	};
+	
+	class U_B_RM_Smock_Cpl: UK3CB_BAF_U_Smock_MTP
+	{
+		author="Steel";
+		scope=2;
+		displayName="Smock [Corporal]";
+		model="\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\suitpack_soldier_RN_co.paa"
+		};
+		class ItemInfo: UniformItem
+		{
+			uniformModel="-";
+			uniformClass="B_RM_Smock_Cpl";
+			containerClass="Supply40";
+			mass=20;
+		};
+	};
+	
+	class U_B_RM_Smock_Sgt: UK3CB_BAF_U_Smock_MTP
+	{
+		author="Steel";
+		scope=2;
+		displayName="Smock [Sergeant]";
+		model="\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\suitpack_soldier_RN_co.paa"
+		};
+		class ItemInfo: UniformItem
+		{
+			uniformModel="-";
+			uniformClass="B_RM_Smock_Sgt";
+			containerClass="Supply40";
+			mass=20;
+		};
+	};
+	
+	class U_B_RM_Smock_CSgt: UK3CB_BAF_U_Smock_MTP
+	{
+		author="Steel";
+		scope=2;
+		displayName="Smock [Colour Sergeant]";
+		model="\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\suitpack_soldier_RN_co.paa"
+		};
+		class ItemInfo: UniformItem
+		{
+			uniformModel="-";
+			uniformClass="B_RM_Smock_CSgt";
+			containerClass="Supply40";
+			mass=20;
+		};
+	};	
+	
+	class U_B_RM_Smock_WO2: UK3CB_BAF_U_Smock_MTP
+	{
+		author="Steel";
+		scope=2;
+		displayName="Smock [WO2]";
+		model="\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\suitpack_soldier_RN_co.paa"
+		};
+		class ItemInfo: UniformItem
+		{
+			uniformModel="-";
+			uniformClass="B_RM_Smock_WO2";
+			containerClass="Supply40";
+			mass=20;
+		};
+	};
+	
+	class U_B_RM_Smock_WO1: UK3CB_BAF_U_Smock_MTP
+	{
+		author="Steel";
+		scope=2;
+		displayName="Smock [WO1]";
+		model="\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\suitpack_soldier_RN_co.paa"
+		};
+		class ItemInfo: UniformItem
+		{
+			uniformModel="-";
+			uniformClass="B_RM_Smock_WO1";
+			containerClass="Supply40";
+			mass=20;
+		};
+	};
+	
+	class U_B_RM_Smock_2Lt: UK3CB_BAF_U_Smock_MTP
+	{
+		author="Steel";
+		scope=2;
+		displayName="Smock [Second Lieutenant]";
+		model="\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\suitpack_soldier_RN_co.paa"
+		};
+		class ItemInfo: UniformItem
+		{
+			uniformModel="-";
+			uniformClass="B_RM_Smock_2Lt";
+			containerClass="Supply40";
+			mass=20;
+		};
+	};
+	
+	class U_B_RM_Smock_Lt: UK3CB_BAF_U_Smock_MTP
+	{
+		author="Steel";
+		scope=2;
+		displayName="Smock [Lieutenant]";
+		model="\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\suitpack_soldier_RN_co.paa"
+		};
+		class ItemInfo: UniformItem
+		{
+			uniformModel="-";
+			uniformClass="B_RM_Smock_Lt";
+			containerClass="Supply40";
+			mass=20;
+		};
+	};
+	
+	class U_B_RM_Smock_Capt: UK3CB_BAF_U_Smock_MTP
+	{
+		author="Steel";
+		scope=2;
+		displayName="Smock [Captain]";
+		model="\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\suitpack_soldier_RN_co.paa"
+		};
+		class ItemInfo: UniformItem
+		{
+			uniformModel="-";
+			uniformClass="B_RM_Smock_Capt";
+			containerClass="Supply40";
+			mass=20;
+		};
+	};
+	
+	class U_B_RM_Smock_Maj: UK3CB_BAF_U_Smock_MTP
+	{
+		author="Steel";
+		scope=2;
+		displayName="Smock [Major]";
+		model="\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\suitpack_soldier_RN_co.paa"
+		};
+		class ItemInfo: UniformItem
+		{
+			uniformModel="-";
+			uniformClass="B_RM_Smock_Maj";
+			containerClass="Supply40";
+			mass=20;
+		};
+	};
+	
+	class U_B_RM_Smock_LtCol: UK3CB_BAF_U_Smock_MTP
+	{
+		author="Steel";
+		scope=2;
+		displayName="Smock [Lieutenant Colonel]";
+		model="\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\suitpack_soldier_RN_co.paa"
+		};
+		class ItemInfo: UniformItem
+		{
+			uniformModel="-";
+			uniformClass="B_RM_Smock_LtCol";
+			containerClass="Supply40";
+			mass=20;
+		};
+	};
+	
+	class U_B_RM_Smock_Col: UK3CB_BAF_U_Smock_MTP
+	{
+		author="Steel";
+		scope=2;
+		displayName="Smock [Colonel]";
+		model="\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\suitpack_soldier_RN_co.paa"
+		};
+		class ItemInfo: UniformItem
+		{
+			uniformModel="-";
+			uniformClass="B_RM_Smock_Col";
+			containerClass="Supply40";
+			mass=20;
+		};
+	};
+	
+	class U_B_RM_Smock_SLt: UK3CB_BAF_U_Smock_MTP
+	{
+		author="Steel";
+		scope=2;
+		displayName="Smock [Sub Lieutenant]";
+		model="\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\suitpack_soldier_RN_co.paa"
+		};
+		class ItemInfo: UniformItem
+		{
+			uniformModel="-";
+			uniformClass="B_RM_Smock_SLt";
+			containerClass="Supply40";
+			mass=20;
+		};
+	};
+	
+	class U_B_RM_Smock_LtRN: UK3CB_BAF_U_Smock_MTP
+	{
+		author="Steel";
+		scope=2;
+		displayName="Smock [Lieutenant RN]";
+		model="\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+		hiddenSelectionsTextures[]=
+		{
+			"\RN_Smock\data\suitpack_soldier_RN_co.paa"
+		};
+		class ItemInfo: UniformItem
+		{
+			uniformModel="-";
+			uniformClass="B_RM_Smock_LtRN";
+			containerClass="Supply40";
+			mass=20;
+		};
+	};
+	
+};
